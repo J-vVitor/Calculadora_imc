0a59e438b36b3c9bba7b53e9be3fe32296b1906f
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-10-05 20:16:36 -0300

    Andamento...

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,45 @@
+
+let btnMan = document.querySelector('.man')
+
+function btnMasculino() {
+    btnMan.classList.toggle('btnClick')
+    let valorBtnMan = document.getElementById('masculino').value
+
+    console.log(valorBtnMan)
+}
+
+let btnFeminina = document.querySelector('.female')
+
+function btnFeminino() {
+    btnFeminina.classList.toggle('clickFeminino')
+    let btnValorFeminino = document.getElementById('feminino').value
+
+    console.log(btnValorFeminino)
+}
+
 function envio() {
-    let inputs = document.getElementsByName('sexo');
-    for (i = 0; i < inputs.length; i++) {
-        if (inputs[i].checked)
-            alert(inputs[i].value)
-    }
-};
+    let idadeValor = document.getElementById('idade').value
 
+    let alturaValor = document.getElementById('altura').value
+    let pesoValor = document.getElementById('peso').value
 
+    let campoTextoidade = document.getElementById('idadeT')
+
+    let campoTextoAltura = document.getElementById('alturaT')
+    let campoTextoPeso = document.getElementById('pesoT')
+    let campoTextoImc = document.getElementById('imcT')
+
+    campoTextoidade.innerHTML = `${idadeValor} anos`;
+    campoTextoAltura.innerHTML = `${alturaValor} cm`;
+    campoTextoPeso.innerHTML = `${pesoValor} kg`;
+
+    let imc = pesoValor / (alturaValor * alturaValor)
+
+    let imcFixed = imc.toFixed(2)
+
+    campoTextoImc.innerHTML = imcFixed
+
+}
 
 
 
