341387d4020085af3cf884effc6e746bd59c85e1
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-10-06 13:16:32 -0300

    alguns ajustes

--- assets/script.js
+++ assets/script.js
@@ -18,26 +18,33 @@ function btnFeminino() {
 }
 
 function envio() {
-    let idadeValor = document.getElementById('idade').value
-
     let alturaValor = document.getElementById('altura').value
     let pesoValor = document.getElementById('peso').value
-
-    let campoTextoidade = document.getElementById('idadeT')
-
-    let campoTextoAltura = document.getElementById('alturaT')
-    let campoTextoPeso = document.getElementById('pesoT')
-    let campoTextoImc = document.getElementById('imcT')
-
-    campoTextoidade.innerHTML = `${idadeValor} anos`;
-    campoTextoAltura.innerHTML = `${alturaValor} cm`;
-    campoTextoPeso.innerHTML = `${pesoValor} kg`;
-
     let imc = pesoValor / (alturaValor * alturaValor)
 
     let imcFixed = imc.toFixed(2)
 
-    campoTextoImc.innerHTML = imcFixed
+    let magreza = document.getElementById('magreza')
+    let normal = document.getElementById('normal')
+    let sobrepeso = document.getElementById('sobrepeso')
+    let ob1 = document.getElementById('ob1')
+    let ob2 = document.getElementById('ob2')
+    let ob3 = document.getElementById('ob3')
+
+    if(imcFixed < 18.5){
+        magreza.style.backgroundColor = '#36b1df'
+    }else if(imc >= 18.5 || imc <= 24.9){
+        normal.style.backgroundColor = '#36b1df'
+    }else if(imc >= 25 || imc <= 29.9){
+        sobrepeso.style.backgroundColor = '#36b1df'
+    }else if(imc >= 30 || imc <= 34.9){
+        ob1.style.backgroundColor = '#36b1df'
+    }else if(imc >= 35 || imc <= 39.9){
+        ob2.style.backgroundColor = '#36b1df'
+    }else if(imc >= 40){
+        ob3.style.backgroundColor = '#36b1df'
+    }
+   
 
 }
 
