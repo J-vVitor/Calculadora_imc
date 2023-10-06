53224f9060582362d0b300e9fc2f1cacec0e68de
João Vitor <132713260+J-vVitor@users.noreply.github.com>
2023-10-06 19:32:04 -0300

    Ajustes finais

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+/* FUNÇÃO DE TROCAR A COR DOS BOTÕES APÓS O CLIQUE*/
 let btnMan = document.querySelector('.man')
 
 function btnMasculino() {
@@ -17,6 +17,30 @@ function btnFeminino() {
     console.log(btnValorFeminino)
 }
 
+
+/* FUNÇÃO PRA ADICIONAR '.' NA ALTURA*/
+let altura = document.getElementById('altura')
+
+altura.addEventListener('keypress', () => {
+    let alturaLength = altura.value.length
+
+    if (alturaLength == 1) {
+        altura.value += '.'
+    }
+})
+
+let peso = document.getElementById('peso')
+
+peso.addEventListener('keypress', () => {
+    let pesoLength = peso.value.length
+
+    if (pesoLength == 3) {
+        peso.value += '.'
+    }
+})
+
+
+/* FUNÇÃO DE ENVIO DE INFORMAÇÕES*/
 function envio() {
     let alturaValor = document.getElementById('altura').value
     let pesoValor = document.getElementById('peso').value
@@ -31,21 +55,35 @@ function envio() {
     let ob2 = document.getElementById('ob2')
     let ob3 = document.getElementById('ob3')
 
-    if(imcFixed < 18.5){
-        magreza.style.backgroundColor = '#36b1df'
-    }else if(imc >= 18.5 || imc <= 24.9){
-        normal.style.backgroundColor = '#36b1df'
-    }else if(imc >= 25 || imc <= 29.9){
-        sobrepeso.style.backgroundColor = '#36b1df'
-    }else if(imc >= 30 || imc <= 34.9){
-        ob1.style.backgroundColor = '#36b1df'
-    }else if(imc >= 35 || imc <= 39.9){
-        ob2.style.backgroundColor = '#36b1df'
-    }else if(imc >= 40){
-        ob3.style.backgroundColor = '#36b1df'
+    if (alturaValor === '' || pesoValor === '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'campos obrigatórios vazios',
+        })
     }
-   
 
+    console.log(imcFixed)
+
+    if (imcFixed < 18.5) {
+        magreza.style.backgroundColor = '#36b1df'
+    } else if (imcFixed >= 18.5 && imcFixed <= 24.9) {
+        normal.style.backgroundColor = '#36b1df'
+    } else if (imcFixed >= 25 && imcFixed <= 29.9) {
+        sobrepeso.style.backgroundColor = '#36b1df'
+    } else if (imcFixed >= 30 && imcFixed <= 34.9) {
+        ob1.style.backgroundColor = '#ff0000'
+    } else if (imcFixed >= 35 && imcFixed <= 39.9) {
+        ob2.style.backgroundColor = '#ff0000'
+    } else if (imcFixed >= 40) {
+        ob3.style.backgroundColor = '#ff0000'
+    }
+
+
+}
+
+function recarregar() {
+    window.location.reload()
 }
 
 
